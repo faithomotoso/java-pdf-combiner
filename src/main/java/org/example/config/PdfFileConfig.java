35fce8a24ccc1b1e175d5415c9ed14c2a1438f9f
Faith Omotoso <faithomotoso@gmail.com>
2022-06-21 08:44:07 +0100

    WIP adding feature to exclude pages using the json config file

--- src/main/java/org/example/config/PdfFileConfig.java
+++ src/main/java/org/example/config/PdfFileConfig.java
@@ -9,6 +9,8 @@ import org.example.utils.AppUtility;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class PdfFileConfig {
 
@@ -21,7 +23,7 @@ public class PdfFileConfig {
                         @JsonProperty("filePath") String filePath,
                         @JsonProperty("excludePages") int[] excludePages) throws IOException {
         if (!AppUtility.isFile(filePath, "pdf")) {
-            throw new IOException("File path [" + filePath + "] should be a pdf");
+            throw new IOException("File [" + filePath + "] should be a pdf");
         }
         this.filePath = filePath;
         this.excludePages = excludePages;
@@ -37,10 +39,43 @@ public class PdfFileConfig {
     public Optional<PDDocument> getDocument() {
         Optional<PDDocument> optionalPDDocument = Optional.empty();
         try {
-            optionalPDDocument = Optional.ofNullable(AppUtility.loadPdfFromPath(getFilePath()));
+            PDDocument pdDocument = AppUtility.loadPdfFromPath(this.getFilePath());
+            if (excludePages != null) {
+                pdDocument = this.removePages(pdDocument, this.excludePages);
+            }
+            System.out.println("Returning for this file: " + this.getFilePath());
+            optionalPDDocument = Optional.ofNullable(pdDocument);
         } catch (IOException e) {
-            e.printStackTrace();
+            System.out.println(e.getLocalizedMessage());
         }
         return optionalPDDocument;
+    }
+
+    private PDDocument removePages(PDDocument document, int[] pages) {
+        Arrays.sort(pages);
+
+        // Page numbers from PDDocument are zero based
+        Set<Integer> pagesSet = Arrays.stream(pages).boxed()
+                                .map(i -> --i)
+                                .collect(Collectors.toSet());
+
+        // Create a new doc and extract pages needed in a loop
+        try {
+            final PDDocument newDocument = new PDDocument();
+            for (int i = 0; i < document.getNumberOfPages(); i++) {
+                if (pagesSet.contains(i)) {
+                    // Skip
+                    continue;
+                }
+                newDocument.addPage(document.getPage(i));
+            }
+
+            return newDocument;
+        } catch (Exception e) {
+            System.out.println("Error removing pages: " + e.getLocalizedMessage());
+        }
+
+        // If an error occurs, return the page back
+        return document;
     }
 }
